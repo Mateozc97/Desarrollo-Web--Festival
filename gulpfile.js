a8abff8b30e0f0b6ef2a39b9da80b38ac149a75d
Mateo Zuluaga <mateozc97@gmail.com>
2023-06-26 23:03:20 -0500

    Realizado seccion principal, vocalista, artistas y parte de la galeria en js

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ const sass = require("gulp-sass")(require('sass'));
 const plumber = require('gulp-plumber');
 
 //Imagenes
+const cache = require('gulp-cache');
+const imagemin = require ('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src('src/scss/**/*.scss')// Identificar el archivo de SASS
@@ -15,24 +18,53 @@ function css(done){
     done();//Callback que avisa a gulp cuiando llegamos al final
 }
 
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'))
+    done();
+}
+
 function versionwebp(done){
 
     const opciones = {
         quality: 50
     };
-
     src('src/img/**/*.{png,jpg}')
         .pipe(webp(opciones) )
         .pipe(dest('build/img') )
     done();
+}
 
+function versionAvif(done){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(opciones) )
+        .pipe(dest('build/img') )
+    done();
+}
+
+function javascript(done){
+    src('src/js/**/*.js')
+    .pipe(dest('build/js'));
+
+    done();
 }
 
 function dev(done){
     watch('src/scss/**/*.scss', css)//Cuando se realizan cambios en app.scss se llama esta funcion para llamar la funcion css.
+    watch('src/js/**/*.js', javascript)//Cuando se realizan cambios en app.scss se llama esta funcion para llamar la funcion css.
     done();
 }
 
 exports.css = css;
+exports.js = javascript;
+exports.imagenes = imagenes;
 exports.versionwebp = versionwebp;
-exports.dev = parallel(versionwebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionwebp, versionAvif, javascript,dev);
